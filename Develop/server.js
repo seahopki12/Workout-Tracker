fe64338170cb233481179a6e94a0fb7fa4a241ab
Sean Hopkins <seahopki12@gmail.com>
2020-12-08 22:30:20 -0500

    created db model and seeded the db

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});

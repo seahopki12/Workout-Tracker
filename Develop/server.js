4dbea985ded5f73757c8d2c99631b62b0e3dd58e
Sean Hopkins <seahopki12@gmail.com>
2020-12-10 19:55:37 -0500

    created functionality to route to exercise html

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
+
+// const db = require("./models");
 
 const app = express();
 
@@ -11,6 +14,66 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(__dirname + '/public/exercise.html', (err) => {
+    if (err) throw err;
+  })
+});
+
+// app.post("/api/workouts", (req, res) => {
+//   db.Workout.create(req)
+//   .then(dbLibrary => {
+//     console.log(dbLibrary);
+//   })
+//   .catch(({message}) => {
+//     console.log(message);
+//   });
+// })
+
+// app.post("/submit", ({body}, res) => {
+//   db.Book.create(body)
+//     .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/books", (req, res) => {
+//   db.Book.find({})
+//     .then(dbBook => {
+//       res.json(dbBook);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/library", (req, res) => {
+//   db.Library.find({})
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+// app.get("/populated", (req, res) => {
+//   db.Library.find({})
+//     .populate("books")
+//     .then(dbLibrary => {
+//       res.json(dbLibrary);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
